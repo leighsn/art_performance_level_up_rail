352de729d6b0b0e5ebb350b10c2a064b6b1738fa
leighsn <leighscherrer@gmail.com>
2016-05-16 16:33:05 -0400

    messing with config files

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Add new mime types for use in respond_to blocks:
-Mime::Type.register "text/calendar", :ics
+Mime::Type.register "text/calendar", :ics unless Mime::Type.lookup_by_extension(:ics)
